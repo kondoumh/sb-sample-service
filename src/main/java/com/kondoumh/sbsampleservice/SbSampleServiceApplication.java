27500075efc676cfb8776e8d0d6286e004ea5918
kondoumh <kondoumh@gmail.com>
2018-09-29 11:21:42 +0900

    generated by Spring Initializr

---
+++ src/main/java/com/kondoumh/sbsampleservice/SbSampleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kondoumh.sbsampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSampleServiceApplication.class, args);
+	}
+}
