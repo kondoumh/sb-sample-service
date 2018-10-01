f0320de8273a6b817c0fe33f730b510fc048917e
kondoumh <kondoumh@gmail.com>
2018-10-02 00:24:20 +0900

    add Controller methods

--- src/main/java/com/kondoumh/sbsampleservice/resource/CustomerController.java
+++ src/main/java/com/kondoumh/sbsampleservice/resource/CustomerController.java
@@ -1,22 +1,58 @@
 package com.kondoumh.sbsampleservice.resource;
 
 import com.kondoumh.sbsampleservice.resource.dto.Customer;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
 
-    @RequestMapping(value = "/usr/{id}")
-    public ResponseEntity<Customer> getCustomer(@PathVariable("id") String id) {
+    @ResponseBody
+    @RequestMapping(value = "/usr/{id}", method = RequestMethod.GET)
+    public Customer getCustomer(@PathVariable("id") Long id) {
 
         Customer c = new Customer();
-        c.setId(1234L);
+        c.setId(id);
         c.setName("Bob");
-        return new ResponseEntity<Customer>(c, HttpStatus.OK);
+        return c;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public List<Customer> findCustomer(@PathVariable("name") String name) {
+        Customer a = new Customer();
+        a.setId(1111L);
+        a.setName(name);
+        Customer b = new Customer();
+        b.setId(2222L);
+        b.setName(name);
+        return Arrays.asList(a, b);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/", method = RequestMethod.POST)
+    public Long registerCustomer(@RequestBody @Validated Customer customer) {
+
+        Long id = Long.valueOf(1234L);
+        Customer c = new Customer();
+        c.setId(id);
+        c.setName(customer.getName());
+        return c.getId();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    public int deleteCustomer(@PathVariable("id") Long id) {
+        return 1;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/", method = RequestMethod.PUT)
+    public int updateCustomer(@RequestBody @Validated Customer customer) {
+        return 1;
     }
 }
