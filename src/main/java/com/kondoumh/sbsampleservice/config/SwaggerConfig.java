22d64a66bff368b31a2b704ab50a72574d82bd83
kondoumh <kondoumh@gmail.com>
2018-10-01 12:42:42 +0900

    add api filter

--- src/main/java/com/kondoumh/sbsampleservice/config/SwaggerConfig.java
+++ src/main/java/com/kondoumh/sbsampleservice/config/SwaggerConfig.java
@@ -1,17 +1,16 @@
 package com.kondoumh.sbsampleservice.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import com.google.common.base.Predicate;
+import static springfox.documentation.builders.PathSelectors.regex;
+import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +21,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(selector())
                 .build()
                 .apiInfo(apiInfo());
     }
@@ -33,5 +32,11 @@ public class SwaggerConfig {
                 .description("SpringBoot Sample API")
                 .version("1.0")
                 .build();
+    }
+
+    private Predicate<String> selector() {
+        return or(
+                regex("/api/.*")
+        );
     }
 }
