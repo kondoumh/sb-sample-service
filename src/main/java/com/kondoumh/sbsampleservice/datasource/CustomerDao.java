578f478254108502bc47cf494feb20364c3d70c3
kondoumh <kondoumh@gmail.com>
2018-10-12 12:37:25 +0900

    add dao methods

--- src/main/java/com/kondoumh/sbsampleservice/datasource/CustomerDao.java
+++ src/main/java/com/kondoumh/sbsampleservice/datasource/CustomerDao.java
@@ -1,4 +1,43 @@
 package com.kondoumh.sbsampleservice.datasource;
 
+import com.kondoumh.sbsampleservice.entiry.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
 public class CustomerDao {
+
+    @Autowired
+    protected MongoOperations mongoOperations;
+
+    public Long register(Customer customer) {
+        mongoOperations.save(customer);
+        return customer.getId();
+    }
+
+    public Customer get(Long id) {
+        return mongoOperations.findOne(buildQueryById(id), Customer.class);
+    }
+
+    private Query buildQueryById(Long id) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("id").is(id));
+        return query;
+    }
+
+    public List<Customer> getAll() {
+        return mongoOperations.findAll(Customer.class);
+    }
+
+    public void update(Customer customer) {
+        mongoOperations.save(customer);
+    }
+
+    public void delete(Long id) {
+        Customer customer = mongoOperations.findOne(buildQueryById(id), Customer.class);
+        mongoOperations.remove(customer);
+    }
 }
---
+++ src/main/java/com/kondoumh/sbsampleservice/entiry/Customer.java
@@ -0,0 +1,9 @@
+package com.kondoumh.sbsampleservice.entiry;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private Long id;
+    private String name;
+}
