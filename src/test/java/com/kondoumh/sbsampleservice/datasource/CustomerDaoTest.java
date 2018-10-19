4beaadfe71b2e17404cbf78e7b354f08eafc24d1
kondoumh <kondoumh@gmail.com>
2018-10-19 12:43:01 +0900

    add test package

---
+++ src/test/java/com/kondoumh/sbsampleservice/datasource/CustomerDaoTest.java
@@ -0,0 +1,14 @@
+package com.kondoumh.sbsampleservice.datasource;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CustomerDaoTest {
+
+    @Test
+    public void testRegister() {
+
+    }
+
+}
