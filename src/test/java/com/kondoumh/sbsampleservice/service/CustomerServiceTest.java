4399e60d79351b7a1be809704a0359a3265bd000
kondoumh <kondoumh@gmail.com>
2018-10-25 22:24:46 +0900

    WIP add unit test

--- src/test/java/com/kondoumh/sbsampleservice/service/CustomerServiceTest.java
+++ src/test/java/com/kondoumh/sbsampleservice/service/CustomerServiceTest.java
@@ -49,6 +49,7 @@ public class CustomerServiceTest {
     public void testGetNotFound() {
         when(dao.get(any(Long.class))).thenReturn(null);
         CustomerDto actual = service.get(1234L);
+
         assertThat(actual).isNull();
         verify(dao, times(1)).get(any(Long.class));
     }
@@ -58,19 +59,43 @@ public class CustomerServiceTest {
         List<Customer> entities = Arrays.asList(new Customer(), new Customer());
         when(dao.findByName(any(String.class))).thenReturn(entities);
         List<CustomerDto> actual = service.findByName("Bob");
+
         assertThat(actual.size()).isEqualTo(2);
         verify(dao, times(1)).findByName(eq("Bob"));
     }
 
     @Test
     public void testRegister() {
+        Customer entity = new Customer();
+        entity.setId(1234L);
+        when(dao.register(any(Customer.class))).thenReturn(entity.getId());
+        CustomerDto dto = new CustomerDto();
+        dto.setId(1234L);
+        Long actual = service.register(dto);
+
+        assertThat(actual).isEqualTo(1234L);
+        verify(dao, times(1)).register(any(Customer.class));
     }
 
     @Test
     public void testUpdate() {
+        Customer entity = new Customer();
+        when(dao.update(any(Customer.class))).thenReturn(1);
+
+        CustomerDto dto = new CustomerDto();
+        dto.setId(1234L);
+        int actual = service.update(dto);
+        assertThat(actual).isEqualTo(1);
+        verify(dao, times(1)).update(any(Customer.class));
     }
 
     @Test
     public void testDelete() {
+        Customer entity = new Customer();
+        when(dao.delete(any(Long.class))).thenReturn(1);
+
+        int actual = service.delete(1234L);
+        assertThat(actual).isEqualTo(1);
+        verify(dao, times(1)).delete(any(Long.class));
     }
 }
