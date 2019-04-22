e6bb5a6bb969258fb5c9f506d1dc3264ff528045
kondoumh <kondoumh@gmail.com>
2019-04-22 23:53:08 +0900

    update sh

--- concourse/tasks/build.sh
+++ concourse/tasks/build.sh
@@ -1,5 +1,4 @@
-#!bin/sh -xe
-pwd
-ls -l
-sb-sample-service/mvnw package
-cp sb-sample-service/target/sb-sample-service.jar build-output/
+#!bin/sh
+set -e
+./mvnw package
+#cp sb-sample-service/target/sb-sample-service.jar build-output/
