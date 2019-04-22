1bd359d40be3498c333c0897830938e19f3e073f
kondoumh <kondoumh@gmail.com>
2019-04-23 00:29:10 +0900

    add it scripts

---
+++ concourse/tasks/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+apk add --no-cache py-pip python-dev libffi-dev openssl-dev gcc libc-dev make curl
+pip install docker-compose
+docker-compose up -d
+sleep 30
+'curl -X POST "http://docker:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"'
+'curl -X GET "http://docker:8080/api/usr/1" -H "accept: */*"'
+docker-compose down
