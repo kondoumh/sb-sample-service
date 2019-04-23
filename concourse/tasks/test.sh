b8ce30a668bdabdbe42c911e0bb9295fe0651882
kondoumh <kondoumh@gmail.com>
2019-04-23 06:39:45 +0000

    update sh

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -6,6 +6,6 @@ hostname -i
 cd sb-sample-service
 docker-compose up -d
 sleep 180
-curl -X POST "http://(hostname -i):8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
-curl -X GET "http://(hostname -i):8080/api/usr/1" -H "accept: */*"
+curl -X POST "http://$(hostname -i):8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
+curl -X GET "http://$(hostname -i):8080/api/usr/1" -H "accept: */*"
 docker-compose down
