28366ba2b54141112f2f782bf0d8fb48161bc6e0
kondoumh <kondoumh@gmail.com>
2019-04-23 18:06:30 +0900

    use dind image

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -xe
-source /docker-lib.sh
-start_docker
-hostname
-hostname -i
+apk add --no-cache py-pip python-dev libffi-dev openssl-dev gcc libc-dev make curl
+pip install docker-compose
 cd sb-sample-service
 docker-compose up -d
 sleep 60
-curl -X POST "http://127.0.0.1:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
-curl -X GET "http://127.0.0.1:8080/api/usr/1" -H "accept: */*"
+curl -X POST "http://docker:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
+curl -X GET "http://docker:8080/api/usr/1" -H "accept: */*"
 docker-compose down
