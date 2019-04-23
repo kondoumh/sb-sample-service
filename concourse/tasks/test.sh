560f24e03d7f73c5ac0ec274becfc1231f968b35
kondoumh <kondoumh@gmail.com>
2019-04-23 10:57:19 +0900

    update sh

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -4,6 +4,6 @@ apk add --no-cache py-pip python-dev libffi-dev openssl-dev gcc libc-dev make cu
 pip install docker-compose
 docker-compose up -d
 sleep 30
-'curl -X POST "http://docker:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"'
-'curl -X GET "http://docker:8080/api/usr/1" -H "accept: */*"'
+curl -X POST "http://docker:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
+curl -X GET "http://docker:8080/api/usr/1" -H "accept: */*"
 docker-compose down
