10636501946014fd7d254b2ade898a08460aa591
kondoumh <kondoumh@gmail.com>
2019-04-23 05:17:10 +0000

    change hostname

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -6,6 +6,6 @@ start_docker
 cd sb-sample-service
 docker-compose up -d
 sleep 30
-curl -X POST "http://docker:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
-curl -X GET "http://docker:8080/api/usr/1" -H "accept: */*"
+curl -X POST "http://localhost:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
+curl -X GET "http://localhost:8080/api/usr/1" -H "accept: */*"
 docker-compose down
