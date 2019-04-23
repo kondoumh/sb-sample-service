5b8e9a10db3e9a296de5179288cc4011e461bf37
kondoumh <kondoumh@gmail.com>
2019-04-23 05:25:28 +0000

    set sleep longer

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -5,7 +5,7 @@ start_docker
 
 cd sb-sample-service
 docker-compose up -d
-sleep 30
+sleep 60
 curl -X POST "http://localhost:8080/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
 curl -X GET "http://localhost:8080/api/usr/1" -H "accept: */*"
 docker-compose down
